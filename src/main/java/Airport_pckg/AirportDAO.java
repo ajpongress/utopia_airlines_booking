d368a51a830778e542e4041cbc86fe7187ead048
AJ Pongress <ajpongress@gmail.com>
2022-10-29 14:19:58 -0700

    - Added transactions and catch statements

--- src/main/java/Airport_pckg/AirportDAO.java
+++ src/main/java/Airport_pckg/AirportDAO.java
@@ -1,7 +1,5 @@
 package Airport_pckg;
 
-import com.mysql.cj.x.protobuf.MysqlxPrepare;
-
 import java.sql.*;
 
 public class AirportDAO {
@@ -9,47 +7,65 @@ public class AirportDAO {
     // Add airport
     public boolean addAirport(String iataIdent, String city, String name, int airportId) throws SQLException, ClassNotFoundException {
 
-        AirportDTO objAirportDTOadd = new AirportDTO(iataIdent, city, name, airportId);
+        boolean success = false;
         Connection conn = null;
+        AirportDTO objAirportDTOadd = new AirportDTO(iataIdent, city, name, airportId);
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             String url = "jdbc:mysql://localhost:3306/utopia";
             conn = DriverManager.getConnection(url, "sstack", "password");
 
             String query = "INSERT INTO tbl_airport(iataIdent, city, name, airportId) VALUES ( ?, ?, ?, ?)";
+
+            conn.setAutoCommit(false);
             PreparedStatement prepstmt = conn.prepareStatement(query);
             prepstmt.setString(1, objAirportDTOadd.getIataIdent());
             prepstmt.setString(2, objAirportDTOadd.getCity());
             prepstmt.setString(3, objAirportDTOadd.getName());
             prepstmt.setInt(4, objAirportDTOadd.getAirportId());
             prepstmt.executeUpdate();
-            return true;
+            conn.commit();
+            conn.setAutoCommit(true);
+            success = true;
 
+        } catch (SQLException sqle) {
+            conn.rollback();
+            System.out.println("Invalid SQL query: " + sqle);
         } finally {
             if (conn != null) conn.close();
         }
+        return success;
     }
 
     // Delete Airport based on iataIdent or airportId
     public boolean deleteAirport(String iataIdent,int airportId) throws SQLException, ClassNotFoundException {
 
-        AirportDTO objAirportDTOdelete = new AirportDTO(iataIdent, "", "", airportId);
+        boolean success = false;
         Connection conn = null;
+        AirportDTO objAirportDTOdelete = new AirportDTO(iataIdent, "", "", airportId);
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             String url = "jdbc:mysql://localhost:3306/utopia";
             conn = DriverManager.getConnection(url, "sstack", "password");
 
             String query = "DELETE FROM tbl_airport WHERE iataIdent=? OR airportId=?";
+
+            conn.setAutoCommit(false);
             PreparedStatement prepstmt = conn.prepareStatement(query);
             prepstmt.setString(1, objAirportDTOdelete.getIataIdent());
             prepstmt.setInt(2, objAirportDTOdelete.getAirportId());
             prepstmt.executeUpdate();
-            return true;
+            conn.commit();
+            conn.setAutoCommit(true);
+            success = true;
 
+        } catch (SQLException sqle) {
+            conn.rollback();
+            System.out.println("Invalid SQL query: " + sqle);
         } finally {
             if (conn != null) conn.close();
         }
+        return success;
     }
 
 
