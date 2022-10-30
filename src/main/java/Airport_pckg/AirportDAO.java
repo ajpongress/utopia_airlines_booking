0814b921eac2266669a729b97af278377bea281c
AJ Pongress <ajpongress@gmail.com>
2022-10-29 22:21:08 -0700

    Added update method (updates ICAO code)

--- src/main/java/Airport_pckg/AirportDAO.java
+++ src/main/java/Airport_pckg/AirportDAO.java
@@ -41,7 +41,44 @@ public class AirportDAO {
         return success;
     }
 
-    // Delete Airport based on iataIdent
+
+    // Update Airport - ICAO code - (finds airport based on ICAO code)
+    public boolean updateAirportICAO(String iataIdentOLD, String iataIdentNEW) throws SQLException, ClassNotFoundException {
+
+        // Check for null input
+        if (iataIdentOLD == null || iataIdentNEW == null) {
+            return false;
+        }
+        boolean success = false;
+        Connection conn = null;
+        AirportDTO objAirportDTOupdateOLD = new AirportDTO(iataIdentOLD, "", "", 0);
+        AirportDTO objAirportDTOupdateNEW = new AirportDTO(iataIdentNEW, "", "", 0);
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3306/utopia";
+            conn = DriverManager.getConnection(url, "sstack", "password");
+
+            String query = "UPDATE tbl_airport SET airportId=? WHERE airportId=?";
+
+            conn.setAutoCommit(false);
+            PreparedStatement prepstmt = conn.prepareStatement(query);
+            prepstmt.setString(1, objAirportDTOupdateNEW.getIataIdent());
+            prepstmt.setString(2, objAirportDTOupdateOLD.getIataIdent());
+            prepstmt.executeUpdate();
+            conn.commit();
+            conn.setAutoCommit(true);
+            success = true;
+
+        } catch (SQLException sqle) {
+            conn.rollback();
+            System.out.println("Invalid SQL query: " + sqle);
+        } finally {
+            if (conn != null) conn.close();
+        }
+        return success;
+    }
+
+    // Delete Airport based on ICAO code
     public boolean deleteAirport(String iataIdent) throws SQLException, ClassNotFoundException {
 
         // Check for null input
@@ -56,12 +93,11 @@ public class AirportDAO {
             String url = "jdbc:mysql://localhost:3306/utopia";
             conn = DriverManager.getConnection(url, "sstack", "password");
 
-            String query = "DELETE FROM tbl_airport WHERE iataIdent=? OR airportId=?";
+            String query = "DELETE FROM tbl_airport WHERE iataIdent=?";
 
             conn.setAutoCommit(false);
             PreparedStatement prepstmt = conn.prepareStatement(query);
             prepstmt.setString(1, objAirportDTOdelete.getIataIdent());
-            prepstmt.setInt(2, objAirportDTOdelete.getAirportId());
             prepstmt.executeUpdate();
             conn.commit();
             conn.setAutoCommit(true);
